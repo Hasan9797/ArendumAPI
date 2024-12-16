52c0eaec472915d62ccbdf302cf1c09bfddec305
Hasan <hasanjonsadullayev97@gmail.com>
2024-12-17 00:24:53 +0500

    prisma

---
+++ src/modules/user/user.controller.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from "express";
+
+
+export const getUsers = async (req: Request, res: Response):Promise<void> =>{
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+export const getUserById = async (req: Request, res: Response):Promise<void> =>{
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+
+export const createUser = async (req: Request, res: Response):Promise<void> =>{
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+
+
+const PORT = process.env.PORT || 3000;
+
+//Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//Routes
+// app.use('/api/user',);
+// app.use('/api/client',);
+// app.use('/api/driver',);
+// app.use('/api/deposit',);
+// app.use('/api/payment',);
+// app.use('/api/region',);
+// app.use('/api/structure',);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
